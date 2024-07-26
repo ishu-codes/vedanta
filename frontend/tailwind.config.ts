68e33bc3725430e65c8a014fc347de102553006b
Ishu <kumarishu494@gmail.com>
2024-07-26 12:03:38 +0530

    Initial commit

---
+++ frontend/tailwind.config.ts
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+    theme: {
+        colors: {
+            offWhite: "#ECECEC",
+            offBlack: "#06071D",
+            secondary: "#9C9CB1",
+            inactive: "#626372",
+            translucent: {
+                normal: "#FFFFFF03",
+                hard: "#FFFFFF0a",
+            },
+        },
+        extend: {},
+    },
+    plugins: [],
+};
