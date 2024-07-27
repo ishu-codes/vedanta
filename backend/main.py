9aac30d3086a0184a440f5874bdb0380ec8f9533
Karan Lokchandani <karanlokchandani@protonmail.com>
2024-07-27 09:00:24 +0530

    file upload and embeddings

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,34 @@
-from fastapi import FastAPI
+from vedanta.backend.apps.pdfops import upload_file
+from fastapi import FastAPI, UploadFile, File
 
 app = FastAPI()
 
+
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    """
+    Returns a dictionary containing the title and version of the Project Delta API.
+
+    :return: A dictionary with the keys "title" and "version".
+    :rtype: dict
+    """
+    return {
+        "title": "Project Vedanta",
+        "version": "0.1.0"
+    }
+
+
+@app.get("/health")                                          # Health Check
+@app.post("/upload_pdf/")
+async def upload_pdf(file: UploadFile = File(...)):
+    """
+    Upload a PDF file and process it.
+    and deletes any existing pdf and ppt files.
+
+    Parameters:
+    - file: The UploadFile object representing the PDF file.
+
+    Returns:
+    - The response containing the processed data.
+    """
+    return await upload_file(file)
